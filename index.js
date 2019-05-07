23cae736f6874b214c09cadf8b303fa4956807ae
root <root@localhost.localdomain>
2019-05-07 18:03:32 +0530

    Updated Pagination with middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 const express = require('express');
+const paginate = require('express-paginate');
 let app = express();
 const list = [];
 const client = new Client({
@@ -11,14 +12,20 @@ const client = new Client({
 });
 
 client.connect();
-app.get('/request', (req, res)=>{  
+let countQuery = "SELECT count(*) as cnt FROM data";
+app.use(paginate.middleware(100, 100000));
+app.get('/request', async (req, res, next)=>{
+    
     let limit = req.query.limit;
-    let offset = req.query.pageno * limit;
+    const pageCount = 30000/limit;
+    console.log(req.skip);
+    let offset = req.skip;
     let query = "SELECT * FROM data LIMIT " + limit + " OFFSET " + offset + ";";
-    client.query(query, (err, result) => {
-       if(!err) res.json(result.rows)
-       else res.json({message: "Something went Wrong"})
-    });
+        client.query(query, (err, result1) => {
+        if(!err) res.json({object: 'list', has_more: paginate.hasNextPages(req)(pageCount), length: result1.rowCount})
+        else res.json({message: "Something went Wrong"})
+        });
+    
 });
 
 app.listen(3000, function(){});
